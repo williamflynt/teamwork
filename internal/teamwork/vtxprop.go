42b01b649432f34a5917597c332dc5a22ce8ce83
william <will@getivydata.com>
2022-01-30 17:03:48 -0800

    feat(initial): Add vtxprop.go

---
+++ internal/teamwork/vtxprop.go
@@ -0,0 +1,28 @@
+package teamwork
+
+import "time"
+
+// VertexProperty is any arbitrary property on a Vertex.
+type VertexProperty struct {
+	Value string `json:"value"`
+}
+
+func NewVertexProperty(value string) *VertexProperty {
+	return &VertexProperty{Value: value}
+}
+
+func (v VertexProperty) GetId() string {
+	return v.Value
+}
+
+func (v VertexProperty) Type() string {
+	return "vertexProperty"
+}
+
+func (v VertexProperty) Created() time.Time {
+	return time.Unix(0, 0)
+}
+
+func (v VertexProperty) Modified() time.Time {
+	return time.Unix(0, 0)
+}
